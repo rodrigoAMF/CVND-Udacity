8aecf510cccea0f71ad0a2fef9f1e3cae90fb736
rodrigoAMF <rodrigoamf@outlook.com>
2019-03-09 01:38:24 -0300

    Update project 3 "SLAM" finished

--- P3-SLAM/robot_class.py
+++ P3-SLAM/robot_class.py
@@ -78,18 +78,25 @@ class robot:
             '''
            
         measurements = []
+        i = 0
         
         ## TODO: iterate through all of the landmarks in a world
-        
         ## TODO: For each landmark
-        ## 1. compute dx and dy, the distances between the robot and the landmark
-        ## 2. account for measurement noise by *adding* a noise component to dx and dy
-        ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
-        ##    - Feel free to use the function self.rand() to help calculate this noise component
-        ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
-        ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
-        ##    as list.append([index, dx, dy]), this format is important for data creation done later
-        
+        for landmark in self.landmarks:
+            ## 1. compute dx and dy, the distances between the robot and the landmark
+            dx, dy = landmark[0] - self.x, landmark[1] - self.y
+            ## 2. account for measurement noise by *adding* a noise component to dx and dy
+            ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
+            ##    - Feel free to use the function self.rand() to help calculate this noise component
+            noise = self.rand() * self.measurement_noise
+            dx, dy = dx+noise, dy+noise
+            ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
+            ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
+            ##    as list.append([index, dx, dy]), this format is important for data creation done later
+            if(abs(dx) <= self.measurement_range and abs(dy) <= self.measurement_range):
+                measurements.append([i, dx, dy])
+            i += 1
+            
         ## TODO: return the final, complete list of measurements
         return measurements
 
